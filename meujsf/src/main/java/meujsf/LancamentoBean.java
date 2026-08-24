45fe51a9aace99bdb59bd6bf770af34069716201
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-08-24 15:23:22 -0300

    criando o bean de lancamento e montando a tela

---
+++ meujsf/src/main/java/meujsf/LancamentoBean.java
@@ -0,0 +1,62 @@
+package meujsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+import meujsf.dao.DaoGeneric;
+import meujsf.entidade.Lancamento;
+import meujsf.entidade.Pessoa;
+
+@ViewScoped
+@ManagedBean(name = "lancamentoBean")
+public class LancamentoBean {
+	private Lancamento lancamento = new Lancamento();
+	private DaoGeneric<Lancamento> daoGeneric = new DaoGeneric<>();
+	private List<Lancamento> lancamentos = new ArrayList<>();
+	
+	public String salvar() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = context.getExternalContext();
+		Pessoa user = (Pessoa) externalContext.getSessionMap().get("usuarioLogado");
+		lancamento.setPessoa(user);
+		
+		daoGeneric.merge(lancamento);
+		this.novo();
+		return "";
+	}
+	
+	public String novo() {
+		lancamento = new Lancamento();
+		return "";
+	}
+
+	public Lancamento getLancamento() {
+		return lancamento;
+	}
+
+	public void setLancamento(Lancamento lancamento) {
+		this.lancamento = lancamento;
+	}
+
+	public DaoGeneric<Lancamento> getDaoGeneric() {
+		return daoGeneric;
+	}
+
+	public void setDaoGeneric(DaoGeneric<Lancamento> daoGeneric) {
+		this.daoGeneric = daoGeneric;
+	}
+
+	public List<Lancamento> getLancamentos() {
+		return lancamentos;
+	}
+
+	public void setLancamentos(List<Lancamento> lancamentos) {
+		this.lancamentos = lancamentos;
+	}
+
+}
